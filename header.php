8902610ffbae763bd7f197defc9fd8d798b36138
gabisegger <gabi.segger@gmail.com>
2022-04-01 15:27:40 -0300

    feat: initial configs

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cursos PHP&MYSQL</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <img src="img/logo.png" title="Logo" alt="Logo">
+            <div id="menu"></div>
+        </div>
+    </header>
+
+    <div id="conteudo" class="container">
