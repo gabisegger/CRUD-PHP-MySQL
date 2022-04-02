e64fa9cbcf2c9d1ac532e6aa0a0ae73119e43bd7
gabisegger <gabi.segger@gmail.com>
2022-04-02 20:52:14 -0300

    feat: entering information from MySQL database

--- views/cursos.php
+++ views/cursos.php
@@ -1,1 +1,14 @@
-cursos
+<table style="border:1px solid #ccc; width: 100%">
+    <tr>
+        <th>Nome curso</th>
+        <th>Carga horária</th>
+    </tr>
+
+    <?php
+        while($linha = mysqli_fetch_array($consulta_cursos)){
+            echo '<tr><td>'.$linha['nome_curso'].'</td>';
+            echo '<td>'.$linha['carga_horaria'].'</td></tr>';
+        };
+    ?>
+
+</table>
--- views/matriculas.php
+++ views/matriculas.php
@@ -1,1 +1,14 @@
-matriculas
+<table style="border:1px solid #ccc; width: 100%">
+    <tr>
+        <th>Nome aluno</th>
+        <th>Nome curso</th>
+    </tr>
+
+    <?php
+        while($linha = mysqli_fetch_array($consulta_matriculas)){
+            echo '<tr><td>'.$linha['nome_aluno'].'</td>';
+            echo '<td>'.$linha['nome_curso'].'</td></tr>';
+        };
+    ?>
+
+</table>
