9d5f203e5118963325c42412c81e19a7c5c624b3
gabisegger <gabi.segger@gmail.com>
2022-04-04 14:24:16 -0300

    feat: matriculation registration route

--- index.php
+++ index.php
@@ -14,20 +14,15 @@ else{
     $page = 'home';
 }
 
-if($page == 'cursos'){
-    include 'views/cursos.php';
-}elseif($page == 'alunos'){
-    include 'views/alunos.php';
-}elseif($page == 'matriculas'){
-    include 'views/matriculas.php';
-}elseif($page == 'inserir_curso'){
-    include 'views/inserir_curso.php';
-}elseif($page == 'inserir_aluno'){
-    include 'views/inserir_aluno.php';
-}else{
-    include 'views/home.php';
+switch($page){
+    case 'cursos': include 'views/cursos.php'; break;
+    case 'alunos': include 'views/alunos.php'; break;
+    case 'matriculas': include 'views/matriculas.php'; break;
+    case 'inserir_aluno': include 'views/inserir_aluno.php'; break;
+    case 'inserir_matricula': include 'views/inserir_matricula.php'; break;
+    case 'inserir_curso': include 'views/inserir_curso.php'; break;
+    default: include 'views/home.php'; break;
 }
-
 
 # Rodapé
 include 'footer.php';
